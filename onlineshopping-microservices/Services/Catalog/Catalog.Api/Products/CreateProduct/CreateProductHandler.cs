d1532e25a92b0e412ca3eed4dc68ff0a676ee00f
Ahmed.Samy <Ahmed.Samy@DEV16-PC2>
2025-01-20 04:16:09 +0200

    build catalog.api

---
+++ onlineshopping-microservices/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, string ImageFile, decimal Price, List<string> Category)
+            :ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+                Category = command.Category
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
+
