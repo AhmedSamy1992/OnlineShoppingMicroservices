bb009f40f7ae1306b3ec75a10adb282e726e9585
Ahmed.Samy <Ahmed.Samy@DEV16-PC2>
2025-01-23 05:26:54 +0200

    finish get product by id feature

--- onlineshopping-microservices/Services/Catalog/Catalog.Api/Products/GetProductById/GetProductByIdEndpoint.cs
+++ onlineshopping-microservices/Services/Catalog/Catalog.Api/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,6 +1,27 @@
-﻿namespace Catalog.Api.Products.GetProductById
+﻿using Carter;
+using Catalog.Api.Models;
+using Catalog.Api.Products.GetProducts;
+using Mapster;
+using MediatR;
+
+namespace Catalog.Api.Products.GetProductById
 {
-    public class GetProductByIdEndpoint
+    public record GetProductByIdResponse(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async(Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+                var response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id");
+        }
     }
 }
