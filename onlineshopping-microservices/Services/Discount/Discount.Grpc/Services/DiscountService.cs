62e7f3882f61a2e83138cfb0881bafc7ec54f542
Reem <Reem@DESKTOP-OQO8JHP>
2025-05-10 15:13:43 +0300

    finish  (methods for discount rpc microservice)

--- onlineshopping-microservices/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ onlineshopping-microservices/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,27 +1,70 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services
 {
-    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
     {
-        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
         {
-            return base.GetDiscount(request, context);
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(c => c.ProductName ==  request.ProductName); 
+            if (coupon == null) 
+                coupon = new Coupon { ProductName = "no name", Amount = 0, Description = "no desc"};
+
+            logger.LogInformation("Discount is retrieved for product name {productName} and amount {amount}", coupon.ProductName, coupon.Amount);   
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
         }
 
-        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
         {
-            return base.CreateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "invalid request object"));
+
+            dbContext.Coupons.Add(coupon);
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation("Discount is created for product name {productName} and amount {amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
         }
 
-        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
         {
-            return base.UpdateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "invalid request object"));
+
+            dbContext.Coupons.Update(coupon);
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation("Discount is updated for product name {productName} and amount {amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
         }
 
-        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
         {
-            return base.DeleteDiscount(request, context);
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+            if (coupon == null)
+                coupon = new Coupon { ProductName = "no name", Amount = 0, Description = "no desc" };
+
+            dbContext.Coupons.Remove(coupon);
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation("Discount is removed for product name {productName} and amount {amount}", coupon.ProductName, coupon.Amount);
+
+            return new DeleteDiscountResponse { Success = true };
         }
     }
 }
